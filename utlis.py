067c6ede59c458537a454e5e1412dbf055c13564
magic3007 <maijing3007@qq.com>
2019-05-07 18:23:48 +0800

    commit utlis.py encoder.py

--- utlis.py
+++ utlis.py
@@ -8,8 +8,6 @@ def idct_2d(mat):
     return fftpack.idct(fftpack.idct(mat.T, norm='ortho').T, norm='ortho')
 
 def load_quantization_table(component):
-    # Quantization Table for: Photoshop - (Save For Web 080)
-    # (http://www.impulseadventure.com/photo/jpeg-quantization.html)
     if component == 'lum':
         q = np.array([[2, 2, 2, 2, 3, 4, 5, 6],
                       [2, 2, 2, 2, 3, 4, 5, 6],
@@ -35,6 +33,7 @@ def load_quantization_table(component):
 
     return q
 
+
 def quantize(mat, component):
     q = load_quantization_table(component)
     return (mat / q).round().astype(np.int32)
@@ -42,3 +41,75 @@ def quantize(mat, component):
 def dequantize(mat, component):
     q = load_quantization_table(component)
     return mat * q
+
+def zigzag_points(rows, cols):
+    # constants for directions
+    UP, DOWN, RIGHT, LEFT, UP_RIGHT, DOWN_LEFT = range(6)
+
+    # move the point in different directions
+    def move(direction, point):
+        return {
+            UP: lambda point: (point[0] - 1, point[1]),
+            DOWN: lambda point: (point[0] + 1, point[1]),
+            LEFT: lambda point: (point[0], point[1] - 1),
+            RIGHT: lambda point: (point[0], point[1] + 1),
+            UP_RIGHT: lambda point: move(UP, move(RIGHT, point)),
+            DOWN_LEFT: lambda point: move(DOWN, move(LEFT, point))
+        }[direction](point)
+
+    # return true if point is inside the block bounds
+    def inbounds(point):
+        return 0 <= point[0] < rows and 0 <= point[1] < cols
+
+    # start in the top-left cell
+    point = (0, 0)
+
+    # True when moving up-right, False when moving down-left
+    move_up = True
+
+    for i in range(rows * cols):
+        yield point
+        if move_up:
+            if inbounds(move(UP_RIGHT, point)):
+                point = move(UP_RIGHT, point)
+            else:
+                move_up = False
+                if inbounds(move(RIGHT, point)):
+                    point = move(RIGHT, point)
+                else:
+                    point = move(DOWN, point)
+        else:
+            if inbounds(move(DOWN_LEFT, point)):
+                point = move(DOWN_LEFT, point)
+            else:
+                move_up = True
+                if inbounds(move(DOWN, point)):
+                    point = move(DOWN, point)
+                else:
+                    point = move(RIGHT, point)
+
+def block2zigzag(block):
+    return np.array([block[point] for point in zigzag_points(*block.shape)])
+
+def int2binstr(x, length):
+    return bin(x)[2:][-length:].zfill(length)
+
+# functions for VLI
+
+def bits_required(x):
+    x = abs(x)
+    return 0 if x is 0 else len(bin(x)[2:])
+
+def binstrFlip(s):
+    if not set(s).issubset('01'):
+        raise ValueError("binstr should have only '0's and '1's")
+    return ''.join(map(lambda a: '0' if a is '1' else '0', s))
+
+def int2VLI(x):
+    if x is 0:
+        return ''
+    binstr = bin(abs(x))[2:]
+    if x < 0:
+        binstrFlip(binstr)
+    return binstr
+
